bc9fbe6fc56f70270c8876a1f636f769a1f61d75
ChrisEs146 <chris.es146@gmail.com>
2022-07-25 19:57:35 -0600

    Add: Navigation component

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -1,5 +1,14 @@
 import "./nav.css";
 import logo from "../../images/logo.png";
+const Nav = () => {
+  return (
+    <nav className="navigation">
+      <div className="navigation__logo">
+        <img src={logo} alt="TrackU logo" title="TrackU" />
+      </div>
+      <button className="navigation__btn">Log In</button>
+    </nav>
+  );
 };
 
-export default nav;
+export default Nav;
