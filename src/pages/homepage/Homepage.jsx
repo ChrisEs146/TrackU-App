dad7ae5e90735e64633a21ca99a150424aa23d77
ChrisEs146 <chris.es146@gmail.com>
2022-07-25 19:59:06 -0600

    Add: Hompage component

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,3 +1,29 @@
 import Nav from "../../components/Nav/nav";
 import hero from "../../images/hero.svg";
 import "./homepage.css";
+const Homepage = () => {
+  return (
+    <header className="homepage">
+      <Nav />
+      <div className="homepage__hero-container">
+        <div className="homepage__information">
+          <h1 className="homepage__title">Keep track of your projects</h1>
+          <p className="homepage__text">
+            <span>TrackU</span> gives you an environment where you can create, manage and keep your
+            personal projects updated until completion. Makes your managing process easier.
+          </p>
+          <button className="homepage__btn">Start Now</button>
+        </div>
+        <div className="homepage__image-container">
+          <img
+            src={hero}
+            alt="Illustration of a man with documents around him"
+            className="homepage__image"
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Homepage;
