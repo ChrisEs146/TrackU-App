ab152581ea2c833675fa09f8c39c7a52c8bff02f
ChrisEs146 <chris.es146@gmail.com>
2022-07-30 22:49:01 -0600

    Add: Form component

---
+++ src/pages/form/Form.jsx
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router-dom";
+import "./form.css";
+import Input from "./input/Input";
+
+const Form = () => {
+  const isSignUp = false;
+  const handleChange = () => {};
+  const handleSubmit = () => {};
+
+  return (
+    <section className="registration">
+      <div className="registration__container">
+        <h2 className="registration__title">{isSignUp ? "Sign Up" : "Sign In"}</h2>
+        <form className="registration__form" onSubmit={handleSubmit}>
+          {isSignUp && (
+            <>
+              <Input name="fullName" label="Full Name" type="text" handleChange={handleChange} />
+            </>
+          )}
+          <Input name="email" label="Email Address" type="email" handleChange={handleChange} />
+          <Input name="password" label="Password" type="password" handleChange={handleChange} />
+          {isSignUp && (
+            <>
+              <Input
+                name="confirmPassword"
+                label="Confirm Password"
+                type="password"
+                handleChange={handleChange}
+              />
+            </>
+          )}
+          <button className="registration__btn" type="submit">
+            {isSignUp ? "Sign Up" : "Sign In"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Form;
