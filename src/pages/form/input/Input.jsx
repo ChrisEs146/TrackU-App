c494b5c3a53b79ee8d96ab111b89e104922f5134
ChrisEs146 <chris.es146@gmail.com>
2022-07-30 22:47:05 -0600

    Add: Input component with label and input element
    - This component is going to be used in the form component

---
+++ src/pages/form/input/Input.jsx
@@ -0,0 +1,11 @@
+import "./input.css";
+const Input = ({ name, type, label, handleChange }) => {
+  return (
+    <div className="input__container">
+      <label htmlFor={name}>{label}</label>
+      <input type={type} name={name} id={name} required onChange={handleChange} />
+    </div>
+  );
+};
+
+export default Input;
