b615e24d102c32bb16353aaf11c4c4f7678a22fa
ChrisEs146 <chris.es146@gmail.com>
2022-08-01 17:25:30 -0600

    Update: input element in the Input component

--- src/pages/form/input/Input.jsx
+++ src/pages/form/input/Input.jsx
@@ -12,6 +12,19 @@ const Input = (props) => {
   return (
     <div className="input__container">
       <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        required
+        value={value}
+        placeholder={placeholder}
+        pattern={pattern}
+        focused={focused.toString()}
+        onChange={handleChange}
+        onBlur={handleFocus}
+        onFocus={() => name === "confirmPassword" && setFocused(true)}
+      />
       <span>{errorMsg}</span>
     </div>
   );
