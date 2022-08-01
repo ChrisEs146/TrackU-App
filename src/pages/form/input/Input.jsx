0b9d7e94e390c32a62055566e7cce205e699aca0
ChrisEs146 <chris.es146@gmail.com>
2022-08-01 17:24:40 -0600

    Add: handle focus function
    - It changes the focused state

--- src/pages/form/input/Input.jsx
+++ src/pages/form/input/Input.jsx
@@ -4,6 +4,11 @@ import "./input.css";
 const Input = (props) => {
   const { name, label, errorMsg, type, handleChange, value, placeholder, pattern } = props;
   const [focused, setFocused] = useState(false);
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+
   return (
     <div className="input__container">
       <label htmlFor={name}>{label}</label>
