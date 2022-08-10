b4435cec0f3f4dad8fca756201d0b29690e9099d
ChrisEs146 <chris.es146@gmail.com>
2022-08-09 23:38:58 -0600

    Add: Importing axios and creating base url

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,1 +1,7 @@
+import axios from "axios";
+
+const { REACT_APP_BASE_URL } = process.env;
+const API = axios.create({ baseURL: REACT_APP_BASE_URL });
+
+// Sign In - Sign Up
 export const signIn = (formData) => API.post("/user/signin", formData);
