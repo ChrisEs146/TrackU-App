bb77eea64655c46f427c13372ea509d0efed8685
ChrisEs146 <chris.es146@gmail.com>
2022-08-09 23:40:08 -0600

    Add: Setting the provider

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,8 @@ import "./app.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
