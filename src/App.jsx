86ad2d026ed3ba0e1277bbaef8c57c23725d9f75
ChrisEs146 <chris.es146@gmail.com>
2022-08-12 23:54:29 -0600

    Add: Dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
+import Dashboard from "./pages/dashboard/Dashboard";
 import Form from "./pages/form/Form";
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/registration" element={<Form />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </main>
