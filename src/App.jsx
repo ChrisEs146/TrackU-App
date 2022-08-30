9051f9caa40401fd587dbcb1d36390f214d52bcf
ChrisEs146 <chris.es146@gmail.com>
2022-08-29 18:00:22 -0600

    Add: Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import "react-toastify/dist/ReactToastify.css";
 import Homepage from "./pages/homepage/Homepage";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Form from "./pages/form/Form";
+import Nav from "./components/Nav/nav";
 const App = () => {
   return (
     <main className="app">
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/registration" element={<Form />} />
