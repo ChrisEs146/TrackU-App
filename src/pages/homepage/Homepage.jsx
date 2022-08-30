4e38a625654a6d2d6b4c504cc4d790a7c39753d9
ChrisEs146 <chris.es146@gmail.com>
2022-08-29 18:10:38 -0600

    Update: Replace useNavigate with NavLink

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import Nav from "../../components/Nav/nav";
 import hero from "../../images/hero.svg";
-import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./homepage.css";
 
 const Homepage = () => {
@@ -16,11 +16,9 @@ const Homepage = () => {
             <span>TrackU</span> gives you an environment where you can create, manage and keep your
             personal projects updated until completion. Makes your managing process easier.
           </p>
-          {!user && (
-            <button className="homepage__btn" onClick={() => navigate("/registration")}>
-              Start Now
-            </button>
-          )}
+          <NavLink className="homepage__btn" to="/registration">
+            Start Now
+          </NavLink>
         </div>
         <div className="homepage__image-container">
           <img
