c03e291ad82d46602b80b5eeab6fc193e9e07732
ChrisEs146 <chris.es146@gmail.com>
2022-09-01 17:45:37 -0600

    Add: Sidebar state and state handler
    To be used in the nav component and the dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -6,14 +7,30 @@ import Dashboard from "./pages/dashboard/Dashboard";
 import Form from "./pages/form/Form";
 import Nav from "./components/Nav/nav";
 const App = () => {
+  // Setting the sidebar activation and deactivation state
+  const [isSidebarActive, setIsSidebarActive] = useState(false);
+  /**
+   * Handler to change the sidebar state. It's used in the nav component
+   * and in the sidebar itsef, which is located in the dashboard.
+   */
+  const handleSidebarState = () => setIsSidebarActive(!isSidebarActive);
+
   return (
     <main className="app">
       <BrowserRouter>
-        <Nav />
+        <Nav handleSidebarState={handleSidebarState} />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/registration" element={<Form />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard"
+            element={
+              <Dashboard
+                handleSidebarState={handleSidebarState}
+                isSidebarActive={isSidebarActive}
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
       <ToastContainer />
