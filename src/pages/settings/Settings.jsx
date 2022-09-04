065c33346c96b3d7a35ebc4de04b47f2c8485423
ChrisEs146 <chris.es146@gmail.com>
2022-09-04 17:00:39 -0600

    Add: Settings page

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,8 +1,29 @@
+import { useSelector } from "react-redux";
+import UserCard from "../../components/UserCard/UserCard";
+import { FaUserEdit } from "react-icons/fa";
+import { AiOutlineUserDelete } from "react-icons/ai";
+import "./settings.css";
 const Settings = () => {
+  const { userData } = useSelector((state) => state.user);
+
   return (
-    <div>
+    <section className="settings">
       <h2>Settings</h2>
-    </div>
+      <div className="settings-form-container">
+        <div className="settings__user-info">
+          <UserCard fullName={userData.fullName} />
+          <div className="settings__user-btns">
+            <button className="settings__update-btn">
+              <FaUserEdit /> Update User
+            </button>
+            <button className="settings__delete-btn">
+              <AiOutlineUserDelete /> Delete User
+            </button>
+          </div>
+        </div>
+        <form className="settings__form"></form>
+      </div>
+    </section>
   );
 };
 
