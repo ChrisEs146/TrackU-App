a000c3d05e198babf8c9566129db9cf937613b41
ChrisEs146 <chris.es146@gmail.com>
2022-09-05 16:47:45 -0600

    Add: User delete page

--- src/pages/settings/userDelete/UserDelete.jsx
+++ src/pages/settings/userDelete/UserDelete.jsx
@@ -1,5 +1,65 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { FaLock, FaEnvelope } from "react-icons/fa";
+import FormInput from "../../../components/FormInput/FormInput";
+import "./userDelete.css";
 const UserDelete = () => {
-  return <div>UserDelete</div>;
+  const { userData } = useSelector((state) => state.user);
+  const [deleteFormData, setDeleteFormData] = useState({
+    email: userData.email,
+    password: "",
+  });
+  const formInputs = [
+    {
+      id: 1,
+      name: "email",
+      type: "email",
+      label: "Email Address",
+      icon: <FaEnvelope />,
+      errorMsg: "It must be a valid email!",
+      placeholder: "Email",
+    },
+    {
+      id: 2,
+      name: "confirmPassword",
+      type: "password",
+      label: "Confirm Password",
+      icon: <FaLock />,
+      errorMsg: "Passwords don't match",
+      placeholder: "Confirm Password",
+      pattern: deleteFormData.password,
+    },
+  ];
+
+  const handleChange = (e) => {
+    setDeleteFormData({ ...deleteFormData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="delete__form-container">
+      <div className="delete__form-title">
+        <h2>Delete Account</h2>
+        <p>Confirm your password to delete this account</p>
+      </div>
+      <form className="delete__form">
+        {formInputs.map((input) => {
+          return (
+            <FormInput
+              key={input.id}
+              value={deleteFormData[input.name]}
+              handleChange={handleChange}
+              {...input}
+            />
+          );
+        })}
+        <button className="delete__form-btn" type="submit">
+          Delete Account
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default UserDelete;
