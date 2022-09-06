69a8fc82e506355b68ade9747b9941fb9ae86ec3
ChrisEs146 <chris.es146@gmail.com>
2022-09-06 17:17:21 -0600

    Update: Projects page

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,13 +1,23 @@
 import { FaPlus } from "react-icons/fa";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import "./projects.css";
 const Projects = () => {
   return (
-    <div className="projects">
-      <h2>Projects</h2>
-      <button title="Add Project" aria-label="Button to add project" className="projects__add-btn">
-        <FaPlus />
-      </button>
-    </div>
+    <section className="projects">
+      <div className="projects__title-container">
+        <h2>Projects</h2>
+        <button
+          title="Add Project"
+          aria-label="Button to add project"
+          className="projects__add-btn"
+        >
+          <FaPlus />
+        </button>
+      </div>
+      <div className="projects__container">
+        <ProjectCard />
+      </div>
+    </section>
   );
 };
 
