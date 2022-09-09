5e690dd9c663b5cc5de35d193cb3f3402f731796
ChrisEs146 <chris.es146@gmail.com>
2022-09-08 18:51:48 -0600

    Add: dataCard component

---
+++ src/components/DateItem/DateCard.jsx
@@ -0,0 +1,21 @@
+import "./dateItem.css";
+
+const DateCard = ({ date }) => {
+  const day = date.toLocaleString("default", { day: "2-digit" });
+  const month = date.toLocaleString("default", { month: "short" });
+  const year = date.toLocaleString("default", { year: "numeric" });
+
+  return (
+    <div className="date">
+      <div className="date__container">
+        <div className="date__month-container">
+          <span className="date__month">{month}</span>
+          <span className="date__day">{day}</span>
+        </div>
+        <span className="date__year">{year}</span>
+      </div>
+    </div>
+  );
+};
+
+export default DateCard;
