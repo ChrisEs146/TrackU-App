b5f1013a0c4284827d0b8abc8f37371d3e5f03fe
ChrisEs146 <chris.es146@gmail.com>
2022-09-12 14:37:12 -0600

    Delete: UseSelector hook

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate, NavLink } from "react-router-dom";
 import UserCard from "../UserCard/UserCard";
 import { logOut } from "../../store/slices/user";
