e595c241ae88551d8e946b098ab74ecea28d5177
ChrisEs146 <chris.es146@gmail.com>
2022-09-13 15:36:41 -0600

    Add: Project page imports

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,4 +1,9 @@
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { CircularProgressbarWithChildren, buildStyles } from "react-circular-progressbar";
+import { FaEdit, FaCalendarDay, FaQuestionCircle, FaPlus } from "react-icons/fa";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import UpdateCard from "../../components/UpdateCard/UpdateCard";
+import { progressColor } from "../../Utils/Functions";
 import "./project.css";
 
 const Project = () => {
