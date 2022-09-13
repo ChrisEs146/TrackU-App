052028da08d39cbe709c9f46f68af3d8db628d5f
ChrisEs146 <chris.es146@gmail.com>
2022-09-13 15:42:12 -0600

    Update: ProtectedRoute

--- src/components/Route/ProtectedRoute.jsx
+++ src/components/Route/ProtectedRoute.jsx
@@ -1,5 +1,7 @@
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { logOut } from "../../store/slices/user";
+import jwt_decode from "jwt-decode";
 
 /**
  * Protected component that serves as a bridge between the
@@ -7,11 +9,21 @@ import { Navigate, Outlet } from "react-router-dom";
  * @returns ProtectedRoute component
  */
 const ProtectedRoute = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { userToken } = useSelector((state) => state.user);
 
   if (!userToken) {
     return <Navigate to="/registration" replace />;
   }
+
+  // if (userToken) {
+  //   const decodedToken = jwt_decode(userToken);
+  //   if (decodedToken.exp * 1000 < new Date().getTime()) {
+  //     dispatch(logOut());
+  //     navigate("/registration");
+  //   }
+  // }
 
   return <Outlet />;
 };
