add3689b5ae469559299b6f0e500db4003c5b212
ChrisEs146 <chris.es146@gmail.com>
2022-09-18 16:41:54 -0600

    Add Modal and AddForm component to projects page

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import AddForm from "../../components/AddForm/AddForm";
+import Modal from "../../components/Modal/Modal";
 import "./projects.css";
+
+/**
+ * Projects page component that displays a list
+ * of projects (ProjectCard component).
+ * @returns Projects page component
+ */
 const Projects = () => {
+  const [isFormActive, setIsFormActive] = useState(false);
+
+  /**
+   * Handler to change the form's state. If active, it's
+   * display as a modal, otherwise it's hidden.
+   */
+  const handleFormActivation = () => setIsFormActive(!isFormActive);
   const proj1 = {
     id: 1,
     title: "Angular Web Application",
@@ -18,6 +34,7 @@ const Projects = () => {
           title="Add Project"
           aria-label="Button to add project"
           className="projects__add-btn"
+          onClick={handleFormActivation}
         >
           <FaPlus />
         </button>
@@ -25,6 +42,9 @@ const Projects = () => {
       <div className="projects__container">
         <ProjectCard project={proj1} />
       </div>
+      <Modal isModalActive={isFormActive}>
+        <AddForm type={"Project"} handleModalActivation={handleFormActivation} />
+      </Modal>
     </section>
   );
 };
