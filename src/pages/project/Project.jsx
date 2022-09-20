fa9d84f7fd2a24bb430a096792eaf3cefca1f296
ChrisEs146 <chris.es146@gmail.com>
2022-09-20 16:59:12 -0600

    Update: project component

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { CircularProgressbarWithChildren, buildStyles } from "react-circular-progressbar";
 import { FaEdit, FaCalendarDay, FaQuestionCircle, FaPlus } from "react-icons/fa";
@@ -6,9 +6,9 @@ import { AiOutlineArrowLeft } from "react-icons/ai";
 import { progressColor } from "../../Utils/Functions";
 import UpdateCard from "../../components/UpdateCard/UpdateCard";
 import Modal from "../../components/Modal/Modal";
-import AddForm from "../../components/AddForm/AddForm";
-import "./project.css";
+import DynamicForm from "../../components/DynamicForm/DynamicForm";
 import { AddFormContext } from "../../contexts/AddFormContext";
+import "./project.css";
 
 const Project = () => {
   const { formStatus, formHandler } = useContext(AddFormContext);
@@ -42,22 +42,6 @@ const Project = () => {
     strokeLinecap: "round",
   });
 
-  const date = new Date().toLocaleDateString("default", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "2-digit",
-  });
-
-  const title = "Navigation is ready to go";
-  const description =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates est totam facere! Nostrum dolor explicabo, obcaecati a culpa magni fugit nisi voluptas eos ad iure maiores vero? Omnis consequuntur at vero suscipit repellat modi, dolorem voluptatum quae alias officia, temporibus voluptatibus quaerat amet nulla sapiente laudantium, impedit rem illo laborum iste magni aut. Tenetur similique quia, distinctio cupiditate nihil, odio quaerat perferendis excepturi praesentium voluptas nostrum harum molestiae provident quisquam id, laudantium quo maxime sed. Quam, vero et. Ipsa, saepe! Expedita dicta quis itaque repudiandae consequuntur modi dolor non voluptas? Quam dicta doloribus corporis, unde doloremque provident possimus quae accusantium!";
-  const title2 = "Issues with modals";
-  const description2 =
-    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum numquam sit iure ut perferendis exercitationem repellat ea esse labore consectetur?";
-  const title3 = "Hero section finished";
-  const description3 =
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, repellat?";
-
   return (
     <section className="projectPage">
       <div className="projectPage__main-content">
@@ -72,6 +56,7 @@ const Project = () => {
             title="Edit Project"
             aria-label="Project edit button"
             className="projectPage__edit-btn"
+            onClick={handleProjectUpdateActivation}
           >
             <FaEdit />
           </button>
