8e9f9f2871e6c523dcca1ed2c21f132e52942919
ChrisEs146 <chris.es146@gmail.com>
2022-09-22 17:14:16 -0600

    Update: UpdateCard component

--- src/components/UpdateCard/UpdateCard.jsx
+++ src/components/UpdateCard/UpdateCard.jsx
@@ -2,35 +2,52 @@ import { useState } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import "./updateCard.css";
 
-const UpdateCard = ({ date, title, description }) => {
+const UpdateCard = ({ date, title, description, handleConfirmModal, handleFormActivation }) => {
   const [isOptionsActive, setIsOptionsActive] = useState(false);
-
   const handleOptionsActive = () => setIsOptionsActive(!isOptionsActive);
 
   return (
-    <article className="update">
-      <div
-        className={
-          isOptionsActive ? "update__options-container active" : "update__options-container"
-        }
-      >
-        <button className="update__options-btn">Edit</button>
-        <button className="update__options-btn">Delete</button>
-      </div>
-      <div className="update__date-container">
-        <span>{date}</span>
-        <button
-          title="Show Options"
-          aria-label="Button to show update options"
-          className="update__btn"
-          onClick={handleOptionsActive}
+    <>
+      <article className="update">
+        <div
+          className={
+            isOptionsActive ? "update__options-container active" : "update__options-container"
+          }
         >
-          <BsThreeDotsVertical />
-        </button>
-      </div>
-      <h3 className="update__title">{title}</h3>
-      <p className="update__description">{description}</p>
-    </article>
+          <button
+            className="update__options-btn"
+            onClick={() => {
+              handleOptionsActive();
+              handleFormActivation();
+            }}
+          >
+            Edit
+          </button>
+          <button
+            className="update__options-btn"
+            onClick={() => {
+              handleOptionsActive();
+              handleConfirmModal();
+            }}
+          >
+            Delete
+          </button>
+        </div>
+        <div className="update__date-container">
+          <span>{date}</span>
+          <button
+            title="Show Options"
+            aria-label="Button to show update options"
+            className="update__btn"
+            onClick={handleOptionsActive}
+          >
+            <BsThreeDotsVertical />
+          </button>
+        </div>
+        <h3 className="update__title">{title}</h3>
+        <p className="update__description">{description}</p>
+      </article>
+    </>
   );
 };
 
