cece6ad5b69c473164765f310fad8e62ac62f4f2
ChrisEs146 <chris.es146@gmail.com>
2022-09-22 17:10:45 -0600

    Update: getProcessColor utility function

--- src/Utils/Functions.jsx
+++ src/Utils/Functions.jsx
@@ -5,7 +5,7 @@
  * @returns Hex code to change the progress number's color
  * according to the project's progress.
  */
-export const progressColor = (value) => {
+export const getProgressColor = (value) => {
   if (value >= 80 && value <= 100) {
     return "#3EC70B";
   } else if (value >= 40 && value < 80) {
