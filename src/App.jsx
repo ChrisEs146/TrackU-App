855fbf5f73da8b350c879367b04c4a978376beb9
ChrisEs146 <chris.es146@gmail.com>
2022-10-02 10:28:29 -0600

    Update: App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Homepage from "./pages/homepage/Homepage";
 import Dashboard from "./pages/dashboard/Dashboard";
@@ -11,7 +11,6 @@ import ChangePassword from "./pages/settings/changePassword/ChangePassword";
 import Projects from "./pages/projects/Projects";
 import Project from "./pages/project/Project";
 import ProtectedRoute from "./components/Route/ProtectedRoute";
-import { SidebarProvider } from "./contexts/SidebarContext";
 import "react-toastify/dist/ReactToastify.css";
 import { ProjectList } from "./pages/projectList/ProjectList";
 import DynamicForm from "./components/DynamicForm/DynamicForm";
@@ -19,46 +18,36 @@ import DynamicForm from "./components/DynamicForm/DynamicForm";
 const App = () => {
   return (
     <main className="app">
-      <BrowserRouter>
-        <SidebarProvider>
-          <Nav />
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="/registration" element={<Form />} />
-            <Route element={<ProtectedRoute />}>
-              <Route path="/dashboard" element={<Dashboard />}>
-                <Route index element={<Projects />} />
-                <Route path="projects" element={<Projects />}>
-                  <Route index element={<ProjectList />} />
-                  <Route
-                    path="add-project"
-                    element={<DynamicForm type="Project" editMode={false} />}
-                  />
-                  <Route path=":projectId" element={<Project />} />
-                  <Route
-                    path="edit-project/:projectId"
-                    element={<DynamicForm type="Project" editMode={true} />}
-                  />
-                  <Route
-                    path="add-update"
-                    element={<DynamicForm type="Update" editMode={false} />}
-                  />
-                  <Route
-                    path="edit-update/:updateId"
-                    element={<DynamicForm type="Update" editMode={true} />}
-                  />
-                </Route>
-                <Route path="settings" element={<Settings />}>
-                  <Route index element={<UserUpdate />} />
-                  <Route path="update-user" element={<UserUpdate />} />
-                  <Route path="delete-user" element={<UserDelete />} />
-                  <Route path="change-password" element={<ChangePassword />} />
-                </Route>
-              </Route>
+      <Nav />
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="/registration" element={<Form />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route index element={<Projects />} />
+            <Route path="projects" element={<Projects />}>
+              <Route index element={<ProjectList />} />
+              <Route path="add-project" element={<DynamicForm type="Project" editMode={false} />} />
+              <Route path=":projectId" element={<Project />} />
+              <Route
+                path="edit-project/:projectId"
+                element={<DynamicForm type="Project" editMode={true} />}
+              />
+              <Route path="add-update" element={<DynamicForm type="Update" editMode={false} />} />
+              <Route
+                path="edit-update/:updateId"
+                element={<DynamicForm type="Update" editMode={true} />}
+              />
             </Route>
-          </Routes>
-        </SidebarProvider>
-      </BrowserRouter>
+            <Route path="settings" element={<Settings />}>
+              <Route index element={<UserUpdate />} />
+              <Route path="update-user" element={<UserUpdate />} />
+              <Route path="delete-user" element={<UserDelete />} />
+              <Route path="change-password" element={<ChangePassword />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
       <ToastContainer />
     </main>
   );
