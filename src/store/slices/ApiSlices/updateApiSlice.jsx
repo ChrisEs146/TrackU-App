2d1a0b9fe10c5d2be1e30fe5ce7ddfa2c4f42c51
ChrisEs146 <chris.es146@gmail.com>
2022-10-03 17:35:32 -0600

    Add: Endpoints to updateApiSlice

---
+++ src/store/slices/ApiSlices/updateApiSlice.jsx
@@ -0,0 +1,30 @@
+import { apiSlice } from "./apiSlice";
+
+export const updateApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUpdates: builder.query({
+      query: (projectId) => ({
+        url: `/updates/${projectId}`,
+      }),
+    }),
+    addUpdate: builder.mutation({
+      query: (projectId, updateData) => ({
+        url: `/updates/${projectId}`,
+        method: "POST",
+        body: { ...updateData },
+      }),
+    }),
+    getUpdate: builder.query({
+      query: (projectId, updateId) => ({
+        url: `/updates/project/${projectId}/update/${updateId}`,
+      }),
+    }),
+    editUpdate: builder.mutation({
+      query: (projectId, updateId, updateInfo) => ({
+        url: `updates/project/${projectId}/update/${updateId}`,
+        method: "PUT",
+        body: { ...updateInfo },
+      }),
+    }),
+  }),
+});
