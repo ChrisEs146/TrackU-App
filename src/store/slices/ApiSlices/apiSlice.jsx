0e6a15a3751f87d575f6270d9b48c2970baaa798
ChrisEs146 <chris.es146@gmail.com>
2022-10-03 17:29:44 -0600

    Add: Creating private query using baseQuery
    - This private query implements the logic to handle and update access tokens
     with refresh tokens

--- src/store/slices/ApiSlices/apiSlice.jsx
+++ src/store/slices/ApiSlices/apiSlice.jsx
@@ -16,3 +16,26 @@ const baseQuery = fetchBaseQuery({
     return headers;
   },
 });
+
+// Private Query
+const privateQuery = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.originalStatus === 401 && result?.error?.data.message === "Invalid Token") {
+    console.log("Sending refresh token");
+
+    const refreshToken = await baseQuery("/users/refresh", api, extraOptions);
+    console.log(refreshToken);
+
+    if (refreshToken?.data) {
+      api.dispatch(setUserToken({ ...refreshToken.data })); //CHECK USER DATA
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      if (refreshToken?.error?.status === 401) {
+        refreshToken.error.data.message = "Session Expired";
+      }
+      return refreshToken;
+    }
+  }
+  return result;
+};
