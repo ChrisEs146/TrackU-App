ef5d31349e070aa5ad183e87cf41f83a21c46c0a
ChrisEs146 <chris.es146@gmail.com>
2022-10-03 17:26:54 -0600

    Update: redux store

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../store/slices/user";
+import { apiSlice } from "./slices/ApiSlices/apiSlice";
+import userReducer from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     user: userReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
