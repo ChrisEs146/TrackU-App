b104e62766d1b00f32110ee88f77ce0d5f932829
ChrisEs146 <chris.es146@gmail.com>
2022-10-06 17:17:06 -0600

    Update: Project component

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -17,70 +17,80 @@ import "./project.css";
  */
 const Project = () => {
   const { projectId } = useParams();
-  const { data: project, isLoading } = useGetProjectQuery(projectId);
+  const { data: project, isLoading, isSuccess, isError } = useGetProjectQuery(projectId);
   const { data: updates } = useGetUpdatesQuery(projectId);
+  console.log(projectId);
+  console.log(project);
+  console.log(isLoading);
+  console.log(isSuccess);
+  console.log(isError);
 
   //Setting a status color based on project's progress
-  const currentColor = getProgressColor(project.progress);
+  const currentColor = getProgressColor(project?.progress);
   const progressStyles = buildStyles({
     pathColor: `${currentColor}`,
     trailColor: "#CFD2CF",
     strokeLinecap: "round",
   });
 
-  return isLoading ? (
-    <LoadingSpinner />
-  ) : (
-    <section className="projectPage">
-      <div className="projectPage__main-content">
-        <div className="projectPage__btns-container">
-          <NavLink title="Go back" className="projectPage__back-link" to="/dashboard/projects">
-            <span>
-              <AiOutlineArrowLeft />
-              Back
-            </span>
-          </NavLink>
-          <NavLink
-            title="Edit Project"
-            aria-label="Project edit button"
-            className="projectPage__edit-btn"
-            to={`/dashboard/projects/edit-project/${project._id}`}
-          >
-            <FaEdit />
-          </NavLink>
+  let content;
+  if (isLoading) {
+    content = <LoadingSpinner />;
+  } else if (isSuccess) {
+    content = (
+      <section className="projectPage">
+        <div className="projectPage__main-content">
+          <div className="projectPage__btns-container">
+            <NavLink title="Go back" className="projectPage__back-link" to="/dashboard/projects">
+              <span>
+                <AiOutlineArrowLeft />
+                Back
+              </span>
+            </NavLink>
+            <NavLink
+              title="Edit Project"
+              aria-label="Project edit button"
+              className="projectPage__edit-btn"
+              to={`/dashboard/projects/edit-project/${project?._id}`}
+            >
+              <FaEdit />
+            </NavLink>
+          </div>
+          <h2 className="projectPage__title">{project?.title}</h2>
+          <div className="projectPage__status-container">
+            <div className="projectPage__added">
+              <span className="projectPage__status-label">
+                <FaCalendarDay /> Added:
+              </span>
+              <span>{getShortDate(project?.createdAt)}</span>
+            </div>
+            <div className="projectPage__status">
+              <span className="projectPage__status-label">
+                <FaQuestionCircle /> Status:
+              </span>
+              <span>{project?.status}</span>
+            </div>
+          </div>
+          <div className="projectPage__info">
+            <div className="projectPage__description">
+              <p>{project?.description}</p>
+            </div>
+            <div className="projectPage__progress-container">
+              <CircularProgressbarWithChildren value={project?.progress} styles={progressStyles}>
+                <div className="projectPage__progress-text" style={{ color: currentColor }}>
+                  <span>{project?.progress}%</span>
+                  <span>Completed</span>
+                </div>
+              </CircularProgressbarWithChildren>
+            </div>
+          </div>
         </div>
-        <h2 className="projectPage__title">{project.title}</h2>
-        <div className="projectPage__status-container">
-          <div className="projectPage__added">
-            <span className="projectPage__status-label">
-              <FaCalendarDay /> Added:
-            </span>
-            <span>{getShortDate(project.date)}</span>
-          </div>
-          <div className="projectPage__status">
-            <span className="projectPage__status-label">
-              <FaQuestionCircle /> Status:
-            </span>
-            <span>{project.status}</span>
-          </div>
-        </div>
-        <div className="projectPage__info">
-          <div className="projectPage__description">
-            <p>{project.description}</p>
-          </div>
-          <div className="projectPage__progress-container">
-            <CircularProgressbarWithChildren value={project.progress} styles={progressStyles}>
-              <div className="projectPage__progress-text" style={{ color: currentColor }}>
-                <span>{project.progress}%</span>
-                <span>Completed</span>
-              </div>
-            </CircularProgressbarWithChildren>
-          </div>
-        </div>
-      </div>
-      <Updates updates={updates} />
-    </section>
-  );
+        <Updates updates={updates} />
+      </section>
+    );
+  }
+
+  return content;
 };
 
 export default Project;
