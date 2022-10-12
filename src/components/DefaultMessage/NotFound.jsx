8cd9d61bdea4f55c45424e3c435cd7a7a9557dd8
ChrisEs146 <chris.es146@gmail.com>
2022-10-12 17:03:00 -0600

    Add: jsdoc to NotFound component

--- src/components/DefaultMessage/NotFound.jsx
+++ src/components/DefaultMessage/NotFound.jsx
@@ -1,6 +1,12 @@
 import { CgFileAdd } from "react-icons/cg";
 import "./notFound.css";
 
+/**
+ * Used when the project's list or the update's list
+ * is empty.
+ * @param {string} message Message to display in the component.
+ * @returns Not-found component
+ */
 const NotFound = ({ message }) => {
   return (
     <div className="notFound__container">
