8375164917cfba61e981cbc641ebc9afc3c1e4f3
ChrisEs146 <chris.es146@gmail.com>
2022-10-15 17:57:25 -0600

    Add: useFormAction custom hook
    Encapsulates the logic to call the addProject, updateProject, addUpdate and
    editUpdate mutation hooks.

---
+++ src/hooks/useFormAction.jsx
@@ -0,0 +1,96 @@
+import {
+  useAddProjectMutation,
+  useUpdateProjectMutation,
+} from "../store/slices/ApiSlices/projectApiSlice";
+import {
+  useAddUpdateMutation,
+  useEditUpdateMutation,
+} from "../store/slices/ApiSlices/updateApiSlice";
+
+export const useFormAction = (
+  type,
+  editMode,
+  options = { projectId: undefined, updateId: undefined, formData: undefined }
+) => {
+  const [
+    addProject,
+    {
+      isSuccess: isAddProjectSuccess,
+      isError: isAddProjectError,
+      isLoading: isAddProjectLoading,
+      error: addProjectError,
+    },
+  ] = useAddProjectMutation();
+  const [
+    updateProject,
+    {
+      isSuccess: isUpdateProjectSuccess,
+      isError: isUpdateProjectError,
+      isLoading: isUpdateProjectLoading,
+      error: updateProjectError,
+    },
+  ] = useUpdateProjectMutation();
+  const [
+    addUpdate,
+    {
+      isSuccess: isAddUpdateSuccess,
+      isError: isAddUpdateError,
+      isLoading: isAddUpdateLoading,
+      error: addUpdateError,
+    },
+  ] = useAddUpdateMutation();
+  const [
+    editUpdate,
+    {
+      isSuccess: isEditUpdateSuccess,
+      isError: isEditUpdateError,
+      isLoading: isEditUpdateLoading,
+      error: editUpdateError,
+    },
+  ] = useEditUpdateMutation();
+
+  const formActions = {
+    project: {
+      add: {
+        action: function () {
+          addProject(options.formData);
+        },
+        isSuccess: isAddProjectSuccess,
+        isError: isAddProjectError,
+        isLoading: isAddProjectLoading,
+        error: addProjectError,
+      },
+      edit: {
+        action: function () {
+          updateProject(options.projectId, options.formData);
+        },
+        isSuccess: isUpdateProjectSuccess,
+        isError: isUpdateProjectError,
+        isLoading: isUpdateProjectLoading,
+        error: updateProjectError,
+      },
+    },
+    update: {
+      add: {
+        action: function () {
+          addUpdate({ projectId: options.projectId, updateData: options.formData });
+        },
+        isSuccess: isAddUpdateSuccess,
+        isError: isAddUpdateError,
+        isLoading: isAddUpdateLoading,
+        error: addUpdateError,
+      },
+      edit: {
+        action: function () {
+          editUpdate(options.projectId, options.updateId, options.formData);
+        },
+        isSuccess: isEditUpdateSuccess,
+        isError: isEditUpdateError,
+        isLoading: isEditUpdateLoading,
+        error: editUpdateError,
+      },
+    },
+  };
+
+  return formActions[type.toLowerCase()][editMode ? "edit" : "add"];
+};
