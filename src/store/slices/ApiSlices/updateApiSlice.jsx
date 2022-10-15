a96256dd5402880817065d7ee76fdcc9d64cae8a
ChrisEs146 <chris.es146@gmail.com>
2022-10-15 17:53:51 -0600

    Add: obj to query args

--- src/store/slices/ApiSlices/updateApiSlice.jsx
+++ src/store/slices/ApiSlices/updateApiSlice.jsx
@@ -9,7 +9,7 @@ export const updateApiSlice = apiSlice.injectEndpoints({
       providesTags: ["Update"],
     }),
     addUpdate: builder.mutation({
-      query: (projectId, updateData) => ({
+      query: ({ projectId, updateData }) => ({
         url: `/updates/${projectId}`,
         method: "POST",
         body: { ...updateData },
@@ -17,12 +17,12 @@ export const updateApiSlice = apiSlice.injectEndpoints({
       invalidatesTags: ["Update"],
     }),
     getUpdate: builder.query({
-      query: (projectId, updateId) => ({
+      query: ({ projectId, updateId }) => ({
         url: `/updates/project/${projectId}/update/${updateId}`,
       }),
     }),
     editUpdate: builder.mutation({
-      query: (projectId, updateId, updateInfo) => ({
+      query: ({ projectId, updateId, updateInfo }) => ({
         url: `/updates/project/${projectId}/update/${updateId}`,
         method: "PUT",
         body: { ...updateInfo },
@@ -30,7 +30,7 @@ export const updateApiSlice = apiSlice.injectEndpoints({
       invalidatesTags: ["Update"],
     }),
     deleteUpdate: builder.mutation({
-      query: (projectId, updateId) => ({
+      query: ({ projectId, updateId }) => ({
         url: `/updates/project/${projectId}/update/${updateId}`,
         method: "DELETE",
       }),
