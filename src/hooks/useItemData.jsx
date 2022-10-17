7e1bb202f48d896e3e648c60b95203c704bcadd4
ChrisEs146 <chris.es146@gmail.com>
2022-10-17 17:10:05 -0600

    Add: Comments

--- src/hooks/useItemData.jsx
+++ src/hooks/useItemData.jsx
@@ -1,6 +1,20 @@
-import { useGetProjectQuery } from "../store/slices/ApiSlices/projectApiSlice";
-import { useGetUpdateQuery } from "../store/slices/ApiSlices/updateApiSlice";
+import { updateApiSlice } from "../store/slices/ApiSlices/updateApiSlice";
+import { projectApiSlice } from "../store/slices/ApiSlices/projectApiSlice";
 
+/**
+ * Abstracts the use of the getProject and getUpdate queries.
+ * Depending on the type and editmode, this hook could return:
+ *
+ * **Null** => In case editMode is false.
+ *
+ * **Project Data** (Requires projectId) => In case type is project and editMode is true.
+ *
+ * **Update Data** (Requires projectId and updateId) => In case the type is update and editMode is true.
+ * @param {string} type Project | Update
+ * @param {boolean} editMode
+ * @param {string} options
+ * @returns Null | Object with either a project's or update's data.
+ */
 const useItemData = (type, editMode, options = { projectId: undefined, updateId: undefined }) => {
   const { data: resultProject, isSuccess: resultProjectSuccess } = useGetProjectQuery(
     options.projectId
