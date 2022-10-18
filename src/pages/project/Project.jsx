8b6301f932fb278fb4d156200773d80707cc2124
ChrisEs146 <chris.es146@gmail.com>
2022-10-18 16:15:17 -0600

    Update: Project page

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -21,8 +21,11 @@ import "./project.css";
  * @returns Project component
  */
 const Project = () => {
+  // Getting project ID from URL params
   const { projectId } = useParams();
-  const { data: project, isLoading, isSuccess, isError } = useGetProjectQuery(projectId);
+
+  // Getting project data and updates data
+  const { data: project, isLoading, isSuccess } = useGetProjectQuery(projectId);
   const { data: updates } = useGetUpdatesQuery(projectId);
 
   // Destructuring deleteProject mutation
@@ -64,55 +67,90 @@ const Project = () => {
     content = <LoadingSpinner />;
   } else if (isSuccess) {
     content = (
-      <section className="projectPage">
-        <div className="projectPage__main-content">
-          <div className="projectPage__btns-container">
-            <NavLink title="Go back" className="projectPage__back-link" to="/dashboard/projects">
-              <span>
-                <AiOutlineArrowLeft />
-                Back
-              </span>
-            </NavLink>
-            <NavLink
-              title="Edit Project"
-              aria-label="Project edit button"
-              className="projectPage__edit-btn"
-              to={`/dashboard/projects/edit-project/${project?._id}`}
+      <>
+        <section className="projectPage">
+          <div className="projectPage__main-content">
+            <div className="projectPage__btns-container">
+              <NavLink title="Go back" className="projectPage__back-link" to="/dashboard/projects">
+                <span>
+                  <AiOutlineArrowLeft />
+                  Back
+                </span>
+              </NavLink>
+              <button
+                title="Show Options"
+                aria-label="Button to show project options"
+                className="projectPage__options"
+                onClick={handleProjectOptionsActive}
+              >
+                <BsThreeDotsVertical />
+              </button>
+            </div>
+            <div
+              className={
+                isProjectOptionsActive
+                  ? "projectPage__options-container active"
+                  : "projectPage__options-container"
+              }
             >
-              <FaEdit />
-            </NavLink>
+              <NavLink
+                className="projectPage__options-link"
+                to={`/dashboard/projects/edit-project/${project?._id}`}
+              >
+                <span>Edit</span>
+              </NavLink>
+              <button
+                className="projectPage__options-btn"
+                onClick={() => {
+                  handleProjectOptionsActive();
+                  handleConfirmModal();
+                }}
+              >
+                Delete
+              </button>
+            </div>
+            <h2 className="projectPage__title">{project?.title}</h2>
+            <div className="projectPage__status-container">
+              <div className="projectPage__added">
+                <span className="projectPage__status-label">
+                  <FaCalendarDay /> Added:
+                </span>
+                <span>{getShortDate(project?.createdAt)}</span>
+              </div>
+              <div className="projectPage__status">
+                <span className="projectPage__status-label">
+                  <FaQuestionCircle /> Status:
+                </span>
+                <span>{project?.status}</span>
+              </div>
+            </div>
+            <div className="projectPage__info">
+              <div className="projectPage__description">
+                <p>{project?.description}</p>
+              </div>
+              <div className="projectPage__progress-container">
+                <CircularProgressbarWithChildren value={project?.progress} styles={progressStyles}>
+                  <div className="projectPage__progress-text" style={{ color: currentColor }}>
+                    <span>{project?.progress}%</span>
+                    <span>Completed</span>
+                  </div>
+                </CircularProgressbarWithChildren>
+              </div>
+            </div>
           </div>
-          <h2 className="projectPage__title">{project?.title}</h2>
-          <div className="projectPage__status-container">
-            <div className="projectPage__added">
-              <span className="projectPage__status-label">
-                <FaCalendarDay /> Added:
-              </span>
-              <span>{getShortDate(project?.createdAt)}</span>
-            </div>
-            <div className="projectPage__status">
-              <span className="projectPage__status-label">
-                <FaQuestionCircle /> Status:
-              </span>
-              <span>{project?.status}</span>
-            </div>
-          </div>
-          <div className="projectPage__info">
-            <div className="projectPage__description">
-              <p>{project?.description}</p>
-            </div>
-            <div className="projectPage__progress-container">
-              <CircularProgressbarWithChildren value={project?.progress} styles={progressStyles}>
-                <div className="projectPage__progress-text" style={{ color: currentColor }}>
-                  <span>{project?.progress}%</span>
-                  <span>Completed</span>
-                </div>
-              </CircularProgressbarWithChildren>
-            </div>
-          </div>
-        </div>
-        <Updates updates={updates} />
-      </section>
+          <Updates updates={updates} projectId={projectId} />
+        </section>
+        <ConfirmationModal
+          item={itemData}
+          submit={handleDelete}
+          isError={isError}
+          isSuccess={isDeleteProjectSuccess}
+          error={error}
+          isLoading={isDeleteProjectLoading}
+          isModalActive={isConfirmActive}
+          handleModalActivation={handleConfirmModal}
+        />
+      </>
     );
   }
 
