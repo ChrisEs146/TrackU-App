013e1a2fd4215cebea12675e12274a6dc99a9f4c
ChrisEs146 <chris.es146@gmail.com>
2022-10-22 13:12:53 -0600

    Delete: useSelector

--- src/pages/settings/userDelete/UserDelete.jsx
+++ src/pages/settings/userDelete/UserDelete.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 import { FaLock, FaEnvelope } from "react-icons/fa";
 import FormInput from "../../../components/FormInput/FormInput";
 import FormCard from "../../../components/FormCard/FormCard";
