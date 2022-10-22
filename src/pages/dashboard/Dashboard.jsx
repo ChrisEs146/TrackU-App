5f553bd57fa87ffa425432a34cfdb746a2dc713b
ChrisEs146 <chris.es146@gmail.com>
2022-10-22 13:40:33 -0600

    Update: Dashboard component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,8 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Outlet } from "react-router-dom";
-import { useDispatch } from "react-redux";
 import { useGetUserQuery } from "../../store/slices/ApiSlices/userApiSlice";
-import { setUserData } from "../../store/slices/userSlice";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { SidebarContext } from "../../contexts/SidebarContext";
-import LoadingSpinner from "../../components/Spinner/LoadingSpinner";
 import "./dashboard.css";
 
 /**
@@ -14,19 +11,10 @@ import "./dashboard.css";
  * @returns Dashboard Component
  */
 const Dashboard = () => {
-  const dispatch = useDispatch();
   const { sidebarStatus, sidebarHandler } = useContext(SidebarContext);
-  const { data: user, isSuccess, isLoading } = useGetUserQuery("UserInfo");
+  const { data: user } = useGetUserQuery("UserInfo");
 
-  useEffect(() => {
-    if (isSuccess) {
-      dispatch(setUserData(user));
-    }
-  }, [isSuccess, user]);
-
-  return isLoading ? (
-    <LoadingSpinner />
-  ) : (
+  return (
     <div className="dashboard">
       <Sidebar
         handleSidebarState={sidebarHandler}
