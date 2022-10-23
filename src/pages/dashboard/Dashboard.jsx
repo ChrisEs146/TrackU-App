28708cab057d35f12c42a133d0170050f2ad2847
ChrisEs146 <chris.es146@gmail.com>
2022-10-22 18:02:21 -0600

    Update: Dashboard component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { useGetUserQuery } from "../../store/slices/ApiSlices/userApiSlice";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { SidebarContext } from "../../contexts/SidebarContext";
+import { setUserData } from "../../store/slices/userSlice";
+import { useDispatch } from "react-redux";
+import LoadingSpinner from "../../components/Spinner/LoadingSpinner";
 import "./dashboard.css";
 
 /**
@@ -11,19 +14,32 @@ import "./dashboard.css";
  * @returns Dashboard Component
  */
 const Dashboard = () => {
+  const dispatch = useDispatch();
   const { sidebarStatus, sidebarHandler } = useContext(SidebarContext);
-  const { data: user } = useGetUserQuery("UserInfo");
+  const { data: user, isSuccess, isLoading } = useGetUserQuery();
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(setUserData(user));
+    }
+  }, [isSuccess]);
 
   return (
     <div className="dashboard">
-      <Sidebar
-        handleSidebarState={sidebarHandler}
-        isSidebarActive={sidebarStatus}
-        fullName={user?.fullName}
-      />
-      <div className="dashboard__projects-container">
-        <Outlet />
-      </div>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <Sidebar
+            handleSidebarState={sidebarHandler}
+            isSidebarActive={sidebarStatus}
+            fullName={user?.fullName}
+          />
+          <div className="dashboard__projects-container">
+            <Outlet />
+          </div>
+        </>
+      )}
     </div>
   );
 };
