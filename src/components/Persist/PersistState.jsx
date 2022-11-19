c5b2a09c7f8704a349c8e5c6b6a55e2e34647b07
ChrisEs146 <chris.es146@gmail.com>
2022-11-19 16:56:45 -0600

    Update: Component using SessionContext

--- src/components/Persist/PersistState.jsx
+++ src/components/Persist/PersistState.jsx
@@ -1,21 +1,23 @@
 import { useSelector } from "react-redux";
 import { Outlet, Navigate, useLocation } from "react-router-dom";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useContext } from "react";
 import { useRefreshMutation } from "../../store/slices/ApiSlices/userApiSlice";
 import { useLogOutMutation } from "../../store/slices/ApiSlices/userApiSlice";
 import LoadingSpinner from "../../components/Spinner/LoadingSpinner";
 import { toast } from "react-toastify";
+import { SessionContext } from "../../contexts/SessionContext";
+import { clearLocalStorage } from "../../Utils/Functions";
 
 /**
  * Wrapper component who's in charge of mantaining the user's state
  * if the refresh token is still valid.
  */
-const PersistState = ({ logOutState }) => {
-  const { isLoggingOut, setIsLoggingOut } = logOutState;
+const PersistState = () => {
   const { userToken } = useSelector((state) => state.user);
   const [success, setSuccess] = useState(false);
   const useEffectExecuted = useRef(false);
   const location = useLocation();
+  const { sessionStatus, sessionHandler } = useContext(SessionContext);
   const [refresh, { isLoading, isSuccess, isError, isUninitialized }] = useRefreshMutation();
   const [logOut] = useLogOutMutation();
 
@@ -26,15 +28,15 @@ const PersistState = ({ logOutState }) => {
           await refresh().unwrap();
           setSuccess(true);
         } catch (error) {
-          localStorage.clear();
+          clearLocalStorage();
           await logOut();
         }
       };
 
-      if (!userToken && !isLoggingOut) {
+      if (!userToken && !sessionStatus) {
         validateToken();
-      } else if (isLoggingOut) {
-        setIsLoggingOut(false);
+      } else if (sessionStatus) {
+        sessionHandler(false);
       }
     }
 
@@ -49,7 +51,7 @@ const PersistState = ({ logOutState }) => {
   } else if (isError) {
     toast.error("Session Expired");
     content = <Navigate to="/registration" state={{ from: location }} replace />;
-  } else if ((isSuccess && success) || (userToken && isUninitialized)) {
+  } else if ((isSuccess && success && userToken) || (userToken && isUninitialized)) {
     content = <Outlet />;
   }
 
