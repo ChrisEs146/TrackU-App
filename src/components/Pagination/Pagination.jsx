8761c8deffb7d2a5c826e3993c1a27b3051ed747
ChrisEs146 <chris.es146@gmail.com>
2022-11-30 15:57:18 -0600

    Update: pagination component

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -3,38 +3,22 @@ import { addToLocalStorage } from "../../Utils/Functions";
 import "./pagination.css";
 
 /**
- * Renders a pagination component using the required amount of items
- * to be displayed per page, total amount of items in the main array, and
- * a function to update the currentPage.
- * @param {Number} itemsPerPage  Items required to be displayed per page
- * @param {Number} totalItems  Total items in the main array
- * @param {Number} currentPage  Current page value
+ * Renders a pagination component.
  * @param {Function} setCurrentPage  Function to update the current page
- * @param {Function} setOffset  Function to update the offset value
- * @param {string} componetKey  Key to add value to the local storage
+ * @param {Number} currentPage  Current page value
+ * @param {string} componentKey  Key to add value to the local storage
+ * @param {Number} pageCount  Number of pages
  * @returns Pagination component
  */
-const Pagination = ({
-  itemsPerPage,
-  totalItems,
-  currentPage,
-  setCurrentPage,
-  setOffSet,
-  componentKey,
-}) => {
-  const totalPages = Math.ceil(totalItems / itemsPerPage);
-
+const Pagination = ({ setCurrentPage, currentPage, componentKey, pageCount }) => {
   /**
    * Handles page change in the pagination component by setting
    * the currentPage to a new offset value.
    * @param {Number} Selected Selected page in the pagination component
    */
   const handlePageChange = ({ selected }) => {
-    const newOffset = (selected * itemsPerPage) % totalItems;
     setCurrentPage(selected);
-    setOffSet(newOffset);
     addToLocalStorage(`${componentKey}Page`, selected, false);
-    addToLocalStorage(`${componentKey}Offset`, newOffset, false);
   };
 
   return (
@@ -43,10 +27,10 @@ const Pagination = ({
       nextLabel="Next"
       breakLabel="..."
       pageRangeDisplayed={3}
-      pageCount={totalPages}
+      pageCount={pageCount}
       onPageChange={handlePageChange}
-      marginPagesDisplayed={0}
       forcePage={currentPage}
+      marginPagesDisplayed={0}
       containerClassName={"pagination__container"}
       previousClassName={"pagination__prev-link"}
       nextClassName={"pagination__next-link"}
