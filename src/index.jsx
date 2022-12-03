54e83095f1215322aaa9d0e5495f9b6408cee529
ChrisEs146 <chris.es146@gmail.com>
2022-12-02 20:53:55 -0600

    Add: disable devtools in production

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,12 @@ import { store } from "./store/index";
 import { BrowserRouter } from "react-router-dom";
 import { SidebarProvider } from "./contexts/SidebarContext";
 import { SessionProvider } from "./contexts/SessionContext";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import App from "./App";
 import "./app.css";
+import { processResult } from "immer/dist/internal";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
